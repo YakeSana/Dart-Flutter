234b13021c717cc920cbed8c1fde7c19cffa0e01
Mitchell_smk <145414207+YakeSana@users.noreply.github.com>
2024-08-21 13:00:47 -0300

    up

--- ap2/lib.dart
+++ ap2/lib.dart
@@ -2,31 +2,36 @@
 import 'dart:io';
 //importar pacote para geração de valores aleatórios
 import 'dart:math';
+
 //criar um enum que representa as opções, lembrando que o usuário pode querer sair
-enum Numeros{Papel,Tesoura,Pedra,Sair}
+enum Numeros { Papel, Tesoura, Pedra, Sair }
+
 void jogo() {
-var escolha;
+  bool validSair = true;
 //loop que continua enquanto o usuário deseja continuar
-while(escolha != 'Sair'){
+  while (validSair == true) {
 //exibe o menu
-  //captura a opção do usuário,validando
+    stdout.writeln("Escolha Pedra(1), Papel(2), Tesoura(3) ou (4) para sair.");
+    //captura a opção do usuário,validando
+    var escolha = stdin.readLineSync();
+    //se o usuario digitar 4, sair, senão
+    if (escolha == 4) {
+      validSair = false;
+    }
+    //sortear a opção do computador
 
-  //se o usuario digitar 4, sair, senão
+    //mapear opção do usuario para enum
 
-  //sortear a opção do computador
+    // ~~ computador
 
-  //mapear opção do usuario para enum
+    //exibir opcoes
 
-  // ~~ computador
+    //jogador vs computador
 
-  //exibir opcoes
+    //verificar se houve empate ou se alguem venceu
 
-  //jogador vs computador
+    //mostrar resultado,claro
 
-  //verificar se houve empate ou se alguem venceu
-
-  //mostrar resultado,claro
-
-  //a cada rodada, durma 3 segundos para dar tempo de ver o resultado
+    //a cada rodada, durma 3 segundos para dar tempo de ver o resultado
   }
 }
