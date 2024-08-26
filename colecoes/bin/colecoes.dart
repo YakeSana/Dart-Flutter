f4bdc15ac20ad748d7b5ffdb3ae9e18f70be4f97
Mitchell_smk <145414207+YakeSana@users.noreply.github.com>
2024-08-26 09:06:25 -0300

    colecoes

---
+++ colecoes/bin/colecoes.dart
@@ -0,0 +1,58 @@
+import 'package:colecoes/colecoes.dart' as colecoes;
+
+void main(List<String> arguments){
+
+
+  //List<String>? eAgora = null;
+
+  //List<String?>? eAgora = null;
+
+  List<List<String?>>? eAgora = [["Lis1","List2"],["List3","List",null]];
+
+  //type annotation
+  // var nomes = <String?> [];
+  // nomes.add(null);
+
+  // var nomes = [];
+  // nomes.add("ana");
+  // nomes.add(null);
+
+  // List<String?> nomes = ["Ana", "João"];
+  // nomes.add(null);
+
+  // //nomes = ["Pedro"];
+  // nomes[0] = "Jaqueline";
+  // print( nomes);
+
+  // //inferência
+  // var l1 = [2];
+  // //dynamic
+  // List l2 = [2];
+
+  // var letras = ["A", "B"];
+  // print(letras.isEmpty);
+  // print(letras.isNotEmpty);
+  // print(letras.reversed);
+  // print(letras.first);
+  // print(letras.firstOrNull);
+  // print(letras.last);
+  // print(letras.lastOrNull);
+
+  // List<num> numeros = [1,1.1];
+  // numeros.toString();
+  // print(numeros[2]);
+
+  // var nomes = ["Ana", "Pedro", 1];
+  // nomes[1] = 2.4;
+  // nomes[0].falar();
+  //
+  // 
+  // num sum = 0;
+  // for(final arg in arguments){
+  //   sum += int.parse(arg);
+  // };
+  // print(sum);
+
+  // desafio 
+  // estudar como funciona a função for each que as listas 
+}
---
+++ colecoes/test/colecoes_test.dart
@@ -0,0 +1,6 @@
+import 'package:colecoes/colecoes.dart';
+import 'package:test/colecoes_test.dart';
+
+void main() {
+  
+}
