4b0c3b22707b7bd5fa9859caefa818d62fb15266
Mitchell_smk <145414207+YakeSana@users.noreply.github.com>
2024-09-16 12:31:20 -0300

    ap01 flutter final

--- exibe_imagens/lib/src/api.dart
+++
@@ -1,7 +0,0 @@
-class Api{
-  final String api = '91haQVZqEMZbmlE2M14QnXwYEuB6lWRAV1O3Ar2dyjMEbfjddG7mhtvR';
-
-  String getApi(){
-    return api;
-  }
-}
--- exibe_imagens/lib/src/app.dart
+++ exibe_imagens/lib/src/app.dart
@@ -1,73 +1,69 @@
+import 'package:exibe_imagens/model/image_model.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'api.dart';
+import 'widgets/image_list.dart';
+import 'dart:convert';
 
-class App extends StatefulWidget{
+class App extends StatefulWidget {
   const App({super.key});
 
   @override
-  State<App> createState(){
+  State<App> createState() {
     return AppState();
   }
 }
 
+class AppState extends State<App> {
+  int numeroImagens = 0;
+  List<ImageModel> imagens = [];
 
-void obterImagem(){
-  //url builder
-  var api = Api();
-  final url = Uri.https(
-    'api.pexels.com',
-    '/v1/search',
-    {'query':'elevator','page': '1','per_page': '1'},
-  );
-  // http.get(Uri.parse('https://api.pexels.com/v1/search?query=people&per_page=1&page=1'));
-  //request and header auth
-  final request = http.Request('get', url);
-  request.headers.addAll({'Authorization':api.getApi()});
-  request.send().then((res){
-    if(res.statusCode == 200){
-    http.Response.fromStream(res).then((response){
-      print(response.body);
-    });
-    }
-    else{
+  void obterImagem() async {
+    //url builder
+
+    final url = Uri.https(
+      'api.pexels.com',
+      '/v1/search',
+      {'query': 'elevator', 'page': '$numeroImagens', 'per_page': '1'},
+    );
+    // http.get(Uri.parse('https://api.pexels.com/v1/search?query=people&per_page=1&page=1'));
+    //request and header auth
+    final request = await http.Request('get', url);
+    request.headers.addAll({'Authorization': 'api'});
+    final res = await request.send();
+    if (res.statusCode == 200) {
+      final response = await http.Response.fromStream(res);
+      var decodedJSON = json.decode(response.body);
+      var imagem = ImageModel.fromJSON(decodedJSON);
+      setState(() {
+        imagens.add(imagem);
+        numeroImagens++;
+      });
+    } else {
       print('Falhou');
     }
-  });
-}
+  }
 
-
-
-class AppState extends State <App>{
-  int numeroImagens = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text('Hello, Flutter'),),
-        body: const Text(''),
-        floatingActionButton: const FloatingActionButton(
-        onPressed: obterImagem,
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Hello, Flutter'),
+            ),
+            body: ImageList(imagens),
+            floatingActionButton: const FloatingActionButton(
+              onPressed: obterImagem,
 
-        // ) {
-        //   setState((){
-        //     numeroImagens++;
-        //   });
-        // },
-        child: Icon(
-        Icons.add,
-        color: Color.fromARGB(255, 16, 179, 43),
-        size:30,
-
-        ),
-        
-        
-        
-
-      )
-      )
-    
-  );
+              // ) {
+              //   setState((){
+              //     numeroImagens++;
+              //   });
+              // },
+              child: Icon(
+                Icons.add,
+                color: Color.fromARGB(255, 16, 179, 43),
+                size: 30,
+              ),
+            )));
   }
 }
